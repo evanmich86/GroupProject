3c2d898070879637e46e896a7e33d895a404034b
Evan <emchrist@uno.edu>
2020-03-02 10:39:01 -0600

    displaying a high score on end page

--- QuizAppTestSpace/end.js
+++ QuizAppTestSpace/end.js
@@ -1,5 +1,9 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup", () =>{
     console.log(username.value);
     saveScoreBtn.disabled = !username.value;
--- QuizAppTestSpace/game.js
+++ QuizAppTestSpace/game.js
@@ -53,6 +53,7 @@ startGame = () => {
 getNewQuestion = () => {
 
     if(availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
         //go to the end page
         return window.location.assign("/end.html");
     }
